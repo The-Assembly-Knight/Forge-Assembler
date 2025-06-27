1fc69dbc23007dc24b3984ccd69e3859651482c9
The-Assembly-Knight <thecodingknight255@gmail.com>
2025-06-27 12:22:52 -0400

    feat: add decimal and hexadecimal numbers tables

---
+++ tables/numbers_table.s
@@ -0,0 +1,26 @@
+.globl DEC_NUMBERS
+.globl HEX_NUMBERS
+
+.section .rodata
+
+DEC_NUMBERS:
+  .ascii "0"
+  .ascii "1"
+  .ascii "2"
+  .ascii "3"
+  .ascii "4"
+  .ascii "5"
+  .ascii "6"
+  .ascii "7"
+  .ascii "8"
+  .ascii "9"
+  .asciz ""
+
+HEX_NUMBERS:
+  .ascii "a"
+  .ascii "b"
+  .ascii "c"
+  .ascii "d"
+  .ascii "e"
+  .ascii "f"
+  .asciz ""
